11eedd62aa8924fca75d07d1cdf298766ae904f1
JoaoAngelotti <joaovictorangelotti@gmail.com>
2020-02-21 01:21:40 -0300

    primeiro commit

---
+++ ValidadorCnpj/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ValidadorCnpj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mtbCnpj = new System.Windows.Forms.MaskedTextBox();
+            this.btnValidaCnpj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mtbCnpj
+            // 
+            this.mtbCnpj.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.mtbCnpj.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbCnpj.Location = new System.Drawing.Point(108, 80);
+            this.mtbCnpj.Mask = "00.000.000/0000-00";
+            this.mtbCnpj.Name = "mtbCnpj";
+            this.mtbCnpj.Size = new System.Drawing.Size(170, 29);
+            this.mtbCnpj.TabIndex = 0;
+            // 
+            // btnValidaCnpj
+            // 
+            this.btnValidaCnpj.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnValidaCnpj.Location = new System.Drawing.Point(108, 141);
+            this.btnValidaCnpj.Name = "btnValidaCnpj";
+            this.btnValidaCnpj.Size = new System.Drawing.Size(170, 29);
+            this.btnValidaCnpj.TabIndex = 1;
+            this.btnValidaCnpj.Text = "VALIDAR";
+            this.btnValidaCnpj.UseVisualStyleBackColor = false;
+            this.btnValidaCnpj.Click += new System.EventHandler(this.btnValidaCnpj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(399, 265);
+            this.Controls.Add(this.btnValidaCnpj);
+            this.Controls.Add(this.mtbCnpj);
+            this.Name = "Form1";
+            this.Text = "CNPJ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox mtbCnpj;
+        private System.Windows.Forms.Button btnValidaCnpj;
+    }
+}
+
---
+++ ValidadorCnpj/Validacao.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidadorCnpj
+{
+    public class Validacao
+    {
+        public bool validarCnpj(string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int somador;
+            int resto;
+            string digito;
+            string cnpjAux;
+
+            //Para evitar erros!!!
+            cnpj = cnpj.Trim(); //Tira todos os espaçamentos (começo e fim)
+            cnpj = cnpj.Replace(",", "").Replace("/", "").Replace("-", "");  //Somente os numeros
+
+            if(cnpj.Length != 14)
+            {
+                return false;
+            }
+            
+            else
+            {
+                cnpjAux = cnpj.Substring(0, 12);
+                somador = 0;
+
+                for(int i=0; i<12; i++)
+                {
+                    somador += int.Parse(cnpjAux[i].ToString()) * multiplicador1[i];
+                }
+
+                resto = (somador % 11);
+
+                if (resto < 2)
+                    resto = 0;
+
+                else
+                    resto = 11 - resto;
+
+                digito = resto.ToString();
+                cnpjAux = cnpjAux + digito;
+
+                //Agora com 13 numeros
+                somador = 0;
+
+                for (int i = 0; i < 13; i++)
+                {
+                    somador += Convert.ToInt16(cnpjAux[i].ToString()) * multiplicador2[i];
+                }
+
+                resto = (somador % 11);
+
+                if (resto < 2)
+                    resto = 0;
+
+                else
+                    resto = 11 - resto;
+
+                digito = digito + resto.ToString(); //Concatenar o 1° com o 2°
+                return cnpj.EndsWith(digito);   //Se for igual retorna TRUE senão FALSE
+            }
+        }
+    }
+}
